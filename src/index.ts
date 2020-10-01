a6d4b9bf04c8dec318fe46485353ea5d4a3cf363
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-01 12:46:19 +0200

    feat: change init script to check for customElements support | bh | #747

--- src/index.ts
+++ src/index.ts
@@ -11,5 +11,5 @@ export const init = (options?: Options): string => {
 
 /* Auto Generated Below */
 export const include = (): string => {
-  return `<script>!function(n){"function"==typeof define&&define.amd?define(n):n()}((function(){"use strict";!function(){var n,e,t=(n=window.navigator.userAgent||navigator.userAgent,(e=/\\b(MSIE |Trident.*?rv:|Edge\\/)(\\d+)/.exec(n))?parseInt(e[2]):void 0);if(t&&t<=18){var i=document.getElementsByTagName("body")[0],o=document.createElement("script");o.src="https://cdn.ui.porsche.com/browser-notification-banner/notification-banner.min.${version}.js",i.appendChild(o)}}()}));</script>`;
+  return `<script>!function(n){"function"==typeof define&&define.amd?define(n):n()}((function(){"use strict";!function(){if(!("customElements"in window)){var n=document.getElementsByTagName("body")[0],e=document.createElement("script");e.src="https://cdn.ui.porsche.com/browser-notification-banner/notification-banner.min.${version}.js",n.appendChild(e)}}()}));</script>`;
 };
--- src/init.ts
+++ src/init.ts
@@ -5,14 +5,9 @@ import { version } from '../package.json';
   const cdnPath = 'https://cdn.ui.porsche.com/browser-notification-banner';
   // const cdnPath = './cdn';
 
-  const getMSBrowserVersion = (): number | undefined => {
-    const ua = window.navigator.userAgent || navigator.userAgent;
-    const match = /\b(MSIE |Trident.*?rv:|Edge\/)(\d+)/.exec(ua);
-    return match ? parseInt(match[2]) : undefined;
-  };
+  const supportsCustomElements = (): boolean => 'customElements' in window;
 
-  const msBrowserVersion = getMSBrowserVersion();
-  if (msBrowserVersion && msBrowserVersion <= 18) {
+  if (!supportsCustomElements()) {
     const body = document.getElementsByTagName('body')[0];
     const script = document.createElement('script');
     script.src = `${cdnPath}/notification-banner.min.${version}.js`;
