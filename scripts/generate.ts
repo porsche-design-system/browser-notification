9ff09f0b6857ca85a01ac48942c3b7cfc62d6d7e
Bernd Hacker <hacker.bernd@gmail.com>
2020-10-01 12:27:40 +0200

    feat: extend banner by desktop styles with browser logos | bh | #747

--- scripts/generate.ts
+++ scripts/generate.ts
@@ -29,6 +29,7 @@ const generateCssAndHtml = async (): Promise<void> => {
     right: 0;
     box-sizing: border-box;
     z-index: 99999;
+    background: #fff;
   }
   
   #ID > div {
@@ -41,7 +42,6 @@ const generateCssAndHtml = async (): Promise<void> => {
     flex-direction: column;
     align-items: center;
     justify-content: center;
-    background-color: #fff;
     text-align: center;
     font-size: 1rem;
     font-family: PorscheNext-Regular, "Arial Narrow", Arial, sans-serif;
@@ -50,7 +50,7 @@ const generateCssAndHtml = async (): Promise<void> => {
     color: #000;
   }
   
-  #ID > div > svg {
+  #ID > div svg {
     width: 5rem;
     height: 5rem;
     margin: 0 0 .5rem;
@@ -58,7 +58,7 @@ const generateCssAndHtml = async (): Promise<void> => {
     fill: #ff9b00;
   }
   
-  #ID > div > strong {
+  #ID > div strong {
     display: block;
     margin: 0 0 1rem;
     font-size: 1.5rem;
@@ -69,7 +69,7 @@ const generateCssAndHtml = async (): Promise<void> => {
     max-width: 59.25rem;
   }
   
-  #ID > div > p > a {
+  #ID > div a {
     margin: 0;
     padding: 0;
     color: #000;
@@ -79,19 +79,64 @@ const generateCssAndHtml = async (): Promise<void> => {
     transition: color 0.24s ease;
   }
   
-  #ID > div > p > a:hover {
+  #ID > div a:hover {
     color: #d5001c;
+  }
+
+  @media only screen and (min-width: 768px) {
+    #ID > div > svg {
+      width: 8.5rem;
+      height: 8.5rem;
+      margin: 0 0 1rem;
+    }
+
+    #ID > div > strong {
+      font-size: 3rem;
+    }
+    
+    #ID > div > div {
+      margin: 2rem 0 0;
+    }
+    
+    #ID > div > div a {
+      display: inline-block;
+      margin: 0 0 0 4rem;
+      font-weight: normal;
+    }
+
+    #ID > div > div a:first-child {
+      margin: 0;
+    }
+
+    #ID > div > div a i {
+      display: block;
+      height: 100px;
+      width: 100px;
+      background: url(./cdn/browser-icons.png) 0 0;
+      background-size: 300px 100px;
+      margin: 0 0 1rem;
+    }
+
+    #ID > div > div a:nth-child(2) i {
+      background-position: -100px 0;
+    }
+
+    #ID > div > div a:nth-child(3) i {
+      background-position: -200px 0;
+    }
   }
   `)}'.replace(/ID/g, ID);
   // prettier-ignore
   const html = \`${minifyHTML(`<div>
-    <style></style>
+    STYLE
     <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" width="100%" height="100%" focusable="false">
       <path d="M12 3L3 21h18zm0 2.24L19.38 20H4.62z"/>
       <path d="M12.5 15l.5-5h-2l.49 5h1.01zM11 16h2v2h-2z"/>
     </svg>
     CONTENT
-  </div>`).replace('<style></style>', '<style>${css}</style>')}\`.replace('CONTENT', locales[getLang()]);
+  </div>`)
+    .replace('STYLE', '<style>${css}</style>')
+    .replace('CONTENT', '${getContent()}')}\`;
 `;
 
   fs.writeFileSync(targetFile, updateContent(oldContent, newContent));
