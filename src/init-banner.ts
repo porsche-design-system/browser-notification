87f008add798454b641a85308144f4f55c15b65c
Matthias Haegele <matthias.haegele@mhp.com>
2021-01-07 15:08:48 +0100

    fix: browser detection of banner | mh / bh | #728

--- src/init-banner.ts
+++ src/init-banner.ts
@@ -1,7 +1,7 @@
 import { version } from '../package.json';
-import { CDN_PATH, supportsMsBrowser } from './init-helpers';
+import { CDN_PATH, isIEorEdgeHTML } from './init-helpers';
 
-if (!supportsMsBrowser()) {
+if (isIEorEdgeHTML()) {
   const script = document.createElement('script');
   script.src = `${CDN_PATH}/banner.min.${version}.js`;
   document.body.appendChild(script);
--- src/init-helpers.ts
+++ src/init-helpers.ts
@@ -3,8 +3,8 @@ export const CDN_PATH = 'https://cdn.ui.porsche.com/browser-notification';
 
 export const supportsCustomElements = (): boolean => 'customElements' in window;
 
-export const supportsMsBrowser = (): boolean => {
+export const isIEorEdgeHTML = (): boolean => {
   const match = /\b(MSIE |Trident.*?rv:|Edge\/)(\d+)/.exec(window.navigator.userAgent || navigator.userAgent);
   const msBrowserVersion = match ? parseInt(match[2]) : -1;
-  return msBrowserVersion > 18;
+  return msBrowserVersion > 0 && msBrowserVersion <= 18;
 };
