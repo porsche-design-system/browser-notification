74614a6d4c3c97528bcdcf65c86cb25a73d8c7b8
Matthias Haegele <matthias.haegele@mhp.com>
2021-10-15 10:45:31 +0200

    feat: use new helper | mh | #1609

--- src/init-overlay.ts
+++ src/init-overlay.ts
@@ -1,7 +1,7 @@
 import { version } from '../package.json';
-import { CDN_PATH, supportsCustomElements } from './init-helpers';
+import { CDN_PATH, supportsUsedPDSTechnologies } from './init-helpers';
 
-if (!supportsCustomElements()) {
+if (!supportsUsedPDSTechnologies()) {
   const script = document.createElement('script');
   script.src = `${CDN_PATH}/overlay.min.${version}.js`;
   document.body.appendChild(script);
