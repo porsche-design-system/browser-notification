7637bf60d0fbc2d06e577526ca96c296df287d0b
Matthias Haegele <matthias.haegele@mhp.com>
2021-10-15 10:45:14 +0200

    feat: extend detection with pds used browser technologies | mh | #1609

--- src/init-helpers.ts
+++ src/init-helpers.ts
@@ -1,10 +1,13 @@
 // TODO: add staging concept
 export const CDN_PATH = 'https://cdn.ui.porsche.com/browser-notification';
 
-export const supportsCustomElements = (): boolean => 'customElements' in window;
-
 export const isIEorEdgeHTML = (): boolean => {
   const match = /\b(MSIE |Trident.*?rv:|Edge\/)(\d+)/.exec(window.navigator.userAgent || navigator.userAgent);
   const msBrowserVersion = match ? parseInt(match[2]) : -1;
   return msBrowserVersion > 0 && msBrowserVersion <= 18;
+};
+
+// TODO: extend array if new technologies are used
+export const supportsUsedPDSTechnologies = (): boolean => {
+  return ['ResizeObserver', 'IntersectionObserver', 'MutationObserver', 'customElements'].every((x) => x in window);
 };
