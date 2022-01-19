be75699d55bf117814783f48ecf136ca476d7332
Matthias Haegele <matthias.haegele@mhp.com>
2022-01-19 14:40:34 +0100

    feat: fix naming / reduce redundancy | mh | #1717

--- src/overlays/init-cookie-overlay.ts
+++ src/overlays/init-cookie-overlay.ts
@@ -1,8 +1,8 @@
-import { hasCookiesEnables } from './overlay-helpers';
+import { hasCookiesEnabled } from './overlay-helpers';
 import { version } from '../../package.json';
 import { CDN_PATH } from '../utils';
 
-if (!hasCookiesEnables()) {
+if (!hasCookiesEnabled()) {
   const script = document.createElement('script');
   script.src = `${CDN_PATH}/cookie-overlay.min.${version}.js`;
   document.body.appendChild(script);
--- src/overlays/overlay-helpers.ts
+++ src/overlays/overlay-helpers.ts
@@ -3,4 +3,4 @@ export const supportsUsedPDSTechnologies = (): boolean => {
   return !['IntersectionObserver', 'MutationObserver', 'customElements'].some((x) => !(x in window));
 };
 
-export const hasCookiesEnables = (): boolean => window.navigator.cookieEnabled || navigator.cookieEnabled;
+export const hasCookiesEnabled = (): boolean => (window.navigator || navigator).cookieEnabled;
