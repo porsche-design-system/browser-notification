67f9329b1674b4a0231f56accf1252fd8679c19f
Stephan Schröter <stephan.schroeter@mhp.com>
2022-05-23 15:12:24 +0200

    feat: add MIT license and copy compulsory files by rollup to generated npm package | sas | #1872

--- rollup.config.js
+++ rollup.config.js
@@ -1,6 +1,10 @@
+import copy from 'rollup-plugin-copy';
 import json from '@rollup/plugin-json';
 import typescript from '@rollup/plugin-typescript';
 import pkg from './package.json';
+
+const rootDir = '.';
+const outputDir = './dist';
 
 export default [
   {
@@ -26,6 +30,13 @@ export default [
       dir: 'dist/esm',
       format: 'esm',
     },
-    plugins: [typescript(), json()],
+    plugins: [typescript(), json(), copy({
+      targets: [
+        { src: `${rootDir}/LICENSE`, dest: outputDir },
+        { src: `${rootDir}/OSS_NOTICE`, dest: outputDir },
+        { src: `${rootDir}/README.md`, dest: outputDir },
+        { src: `${rootDir}/CHANGELOG.md`, dest: outputDir },
+      ],
+    }),],
   },
 ];
--- scripts/package.ts
+++ scripts/package.ts
@@ -5,18 +5,6 @@ const rootDirectory = path.resolve(__dirname, '..');
 const targetDirectory = path.resolve(rootDirectory, 'dist');
 const packageJson = require('../package.json');
 
-const copyFiles = (files: string[]): void => {
-  for (const file of files) {
-    const source = path.resolve(rootDirectory, file);
-    const target = path.resolve(targetDirectory, path.basename(file));
-    fs.copyFile(source, target, (err) => {
-      if (err) {
-        throw err;
-      }
-    });
-  }
-};
-
 const removePropsFromPackageJson = (props: string[]): void => {
   for (const prop of props) {
     Reflect.deleteProperty(packageJson, prop);
@@ -35,7 +23,6 @@ const savePackageJson = () => {
   fs.writeFileSync(target, data);
 };
 
-copyFiles(['CHANGELOG.md', 'LICENSE']);
 removePropsFromPackageJson(['private', 'scripts', 'devDependencies']);
 addPropsToPackageJson({ main: 'index.js', module: 'esm/index.js', types: 'types/index.d.ts' });
 savePackageJson();
